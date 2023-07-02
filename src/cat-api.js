3f272d6f2162b4b1a44efcbbd690661b7ca2e129
Анна Котляр <kotlyaranya1771@gmail.com>
2023-07-02 14:31:32 +0300

    done
    refract

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,17 +1,26 @@
 import axios from "axios";
 
+const API_KEY = "live_In7QA2yESCTx8J4yXaZg4Sa3TTl06kobaNtcsXw3eR1zKeeXrUyQfnHJbMsyMlCy";
+axios.defaults.headers.common["x-api-key"] = API_KEY;
+
 export function fetchBreeds() {
-  axios.defaults.headers.common["x-api-key"] = "live_In7QA2yESCTx8J4yXaZg4Sa3TTl06kobaNtcsXw3eR1zKeeXrUyQfnHJbMsyMlCy";
-  
   return axios.get("https://api.thecatapi.com/v1/breeds")
     .then(response => {
-      return response.data.map(breed => ({
-        value: breed.id,
-        label: breed.name
-      }));
+      return response.data;
     })
     .catch(error => {
       console.error("Error fetching breeds:", error);
       return [];
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+  return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      return response.data[0];
+    })
+    .catch(error => {
+      console.error("Error fetching cat:", error);
+      return null;
     });
 }
--- src/index.js
+++ src/index.js
@@ -1,144 +1,68 @@
-import { fetchBreeds } from "./cat-api.js";
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import Notiflix from 'notiflix';
 
 const errorEl = document.querySelector('.error');
 const loaderEl = document.querySelector('.loader');
 const selectEl = document.querySelector('.breed-select');
 const infoEl = document.querySelector('.cat-info');
 
+selectEl.onchange = createInfo;
 errorEl.style.display = 'none';
 loaderEl.style.display = 'none';
-selectEl.onchange = makeInf;
 infoEl.style.display = 'none';
 
-
-fetchBreeds()
-  .then(breeds => {
-    breeds.forEach(breed => {
-      const optionEl = document.createElement("option");
-      optionEl.value = breed.value;
-      optionEl.textContent = breed.label;
-      selectEl.appendChild(optionEl);
-    });
-  })
-  .catch(error => {
-    console.error("Error:", error);
-});
-
-function makeInf() {
-  catInfo.style.display = 'none'
-  loadingMessage.style.display = 'block' 
-  fetchCatByBreed(catBreed.value)
-    .then((data) => {
-      loadingMessage.style.display = 'none' 
-      catInfo.style.display = 'block'
-      showCatInfo(data[0]);
+function selectCat() {
+  loaderEl.style.display = 'block';
+  fetchBreeds()
+    .then(data => {
+      loaderEl.style.display = 'none';
+      selectEl.innerHTML = data
+        .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+        .join('');
+      selectEl.style.display = 'block';
     })
     .catch(() => {
-      errorMessage.style.display = 'block';
+      errorEl.style.display = 'block';
+    });
+}
+
+selectCat();
+
+function createInfo() {
+  infoEl.innerHTML = '';
+  infoEl.style.display = 'none';
+  loaderEl.style.display = 'block';
+  fetchCatByBreed(selectEl.value)
+    .then(data => {
+      loaderEl.style.display = 'none';
+      infoEl.style.display = 'block';
+      showCatInfo(data);
+    })
+    .catch(() => {
+      errorEl.style.display = 'block';
       Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
     });
 }
 
+function showCatInfo(catData) {
+  const breed = catData.breeds[0];
+  const imgEl = document.createElement('img');
+  imgEl.src = catData.url;
+  imgEl.alt = breed.name;
+  imgEl.height = 600;
+  imgEl.width = 500;
 
-// import SlimSelect from "slim-select";
-// import Notiflix from "notiflix";
+  const nameEl = document.createElement('h1');
+  nameEl.textContent = breed.name;
 
-// import { fetchBreeds, fetchCatByBreed } from "./cat-api";
-// import { showSpinner, hideSpinner } from "./spinner";
+  const descEl = document.createElement('p');
+  descEl.textContent = breed.description;
 
-// const breedSelect = document.querySelector(".breed-select");
-// const loader = document.querySelector(".loader");
-// const error = document.querySelector(".error");
-// const catInfo = document.querySelector(".cat-info");
-// const spinnerContainer = document.getElementById("spinner-container");
+  const temperamentEl = document.createElement('h2');
+  temperamentEl.textContent = `Temperament: ${breed.temperament}`;
 
-// const slimSelect = new SlimSelect({
-//   select: breedSelect,
-// });
-
-// function showLoader() {
-//   loader.style.display = "block";
-// }
-
-// function hideLoader() {
-//   loader.style.display = "none";
-// }
-
-// function showError(message) {
-//   error.textContent = message;
-//   error.style.display = "block";
-// }
-
-// function hideError() {
-//   error.style.display = "none";
-// }
-
-// function showCatInfo(imageUrl, breedName, description, temperament) {
-//   const img = document.createElement("img");
-//   img.src = imageUrl;
-
-//   const name = document.createElement("p");
-//   name.textContent = `Breed: ${breedName}`;
-
-//   const desc = document.createElement("p");
-//   desc.textContent = `Description: ${description}`;
-
-//   const temp = document.createElement("p");
-//   temp.textContent = `Temperament: ${temperament}`;
-
-//   catInfo.innerHTML = "";
-//   catInfo.appendChild(img);
-//   catInfo.appendChild(name);
-//   catInfo.appendChild(desc);
-//   catInfo.appendChild(temp);
-//   catInfo.style.display = "block";
-// }
-
-// function hideCatInfo() {
-//   catInfo.innerHTML = "";
-//   catInfo.style.display = "none";
-// }
-
-// function handleBreedSelectChange() {
-//   const selectedBreed = breedSelect.selected(); // Змінено з selected() на getSelected()
-//   if (selectedBreed.length > 0) {
-//     showSpinner(spinnerContainer);
-//     fetchCatByBreed(selectedBreed[0].value)
-//       .then((data) => {
-//         hideSpinner(spinnerContainer);
-//         renderCatInfo(data);
-//       })
-//       .catch((error) => {
-//         hideSpinner(spinnerContainer);
-//         showError();
-//         console.error(error);
-//       });
-//   } else {
-//     hideCatInfo();
-//   }
-// }
-
-
-// function initializeApp() {
-//   showSpinner(spinnerContainer);
-
-//   fetchBreeds()
-//     .then((breeds) => {
-//       slimSelect.setData(
-//         breeds.map((breed) => ({
-//           value: breed.id,
-//           text: breed.name,
-//         }))
-//       );
-//     })
-//     .catch((error) => {
-//       showError(error.message);
-//     })
-//     .finally(() => {
-//       hideSpinner(spinnerContainer);
-//     });
-
-//   breedSelect.addEventListener("change", handleBreedSelectChange);
-// }
-
-// initializeApp();
+  infoEl.appendChild(imgEl);
+  infoEl.appendChild(nameEl);
+  infoEl.appendChild(descEl);
+  infoEl.appendChild(temperamentEl);
+}
